cfc1f7195294b272c5d03b97d897374e2ca225b1
HACKER-NAZRUL-420 <riyamaya7162@gmail.com>
2024-12-07 16:34:41 +0600

    Update handleCreateDatabase.js

--- NAZRUL/includes/handle/handleCreateDatabase.js
+++ NAZRUL/includes/handle/handleCreateDatabase.js
@@ -1,6 +1,6 @@
 
 module.exports = function ({ Users, Threads, Currencies }) {
-    const logger =require("../../utils/log.js");
+    const logger =require("../../NAZRUL/utils/log.js");
     return async function ({ event }) {
         const { allUserID, allCurrenciesID, allThreadID, userName, threadInfo } = global.data; 
         const { autoCreateDB } = global.config;
