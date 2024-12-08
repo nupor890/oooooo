72ab7bf13ade8b71a15a2431985a9625d58b3cb2
HACKER-NAZRUL-420 <riyamaya7162@gmail.com>
2024-12-08 20:01:53 +0600

    Update model.js

--- NAZRUL/includes/database/model.js
+++ NAZRUL/includes/database/model.js
@@ -1,9 +1,9 @@
 module.exports = function (input) {
 	const force = false;
 
-	const Users = require("./models/users")(input);
-	const Threads = require("./models/threads")(input);
-	const Currencies = require("./models/currencies")(input);
+	const Users = require("./NAZRUL/includes/database/models/users")(input);
+	const Threads = require("./NAZRUL/includes/database/models/threads")(input);
+	const Currencies = require("./NAZRUL/includes/database/models/currencies")(input);
 
 	Users.sync({ force });
 	Threads.sync({ force });
