ee8a6e6d550e799e30a3e7b4f55cb816f3077a17
HACKER-NAZRUL-420 <riyamaya7162@gmail.com>
2024-12-08 19:52:37 +0600

    Update TINA.js

--- TINA.js
+++ TINA.js
@@ -34,7 +34,7 @@ global.data = new Object({
     allThreadID: new Array()
 });
 
-global.utils = require("./NAZRUL/utils");
+global.utils = require("./utils");
 
 global.nodemodule = new Object();
 
