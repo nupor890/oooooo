dff70c47235f7af39b8f8b5ae9f353e32d3c5c87
HACKER-NAZRUL-420 <riyamaya7162@gmail.com>
2024-12-10 22:05:19 +0600

    Update index.js

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function startBot(message) {
 //========= Check update from Github =========//
 ////////////////////////////////////////////////
 
-axios.get("https://raw.githubusercontent.com/priyanshu192/bot/main/package.json")
+axios.get("https://raw.githubusercontent.com/HACKER-NAZRUL-420/TINA/main/package.json")
     .then((res) => {
         logger(res.data.name, "[ NAME ]");
         logger(`Version: ${res.data.version}`, "[ VERSION ]");
