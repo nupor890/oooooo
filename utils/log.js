4c6af85a02cf4e290d7cb00781b35341935c711f
HACKER-NAZRUL-420 <riyamaya7162@gmail.com>
2024-12-12 21:46:57 +0600

    Update log.js

--- utils/log.js
+++ utils/log.js
@@ -24,13 +24,13 @@ module.exports = (data, option) => {
 module.exports.loader = (data, option) => {
   switch (option) {
     case "warn":
-      console.log(chalk.bold.hex(randomColor()).bold("NAZRUL ERROR➜ ") + chalk.bold.hex("#8B8878").bold(data)+chalk.bold.hex("FF00DD")("" ))
+      console.log(chalk.bold.hex(randomColor()).bold(" [ NAZRUL ISLAM ] ") + chalk.bold.hex("#8B8878").bold(data)+chalk.bold.hex("FF00DD")("\n✧➜  ✧ ✧ ✧ ✧ ✧ ✧ NAZRUL ISLAM LIST (👑)✧ ✧ ✧ ✧ ✧ ✧ ✧ ✧ ✧" ))
       break;
     case "error":
-    console.log(chalk.bold.hex(randomColor()).bold("NAZRUL ERROR➜ ") + data+chalk.bold.hex("5EFF00")("" ));
+    console.log(chalk.bold.hex(randomColor()).bold(" [ NAZRUL ISLAM ] ") + data+chalk.bold.hex("5EFF00")("\n✧➜  ✧ ✧ ✧ ✧ ✧ ✧ NAZRUL ISLAM LISY (👑)✧ ✧ ✧ ✧ ✧ ✧ ✧ ✧ ✧" ));
       break;
     default:
-      console.log(chalk.bold.hex(randomColor()).bold(" NAZRUL➜ ") + chalk.bold.hex(randomColor()).bold(data) +chalk.bold.hex("FFF0000")("" ));
+      console.log(chalk.bold.hex(randomColor()).bold(" [ NAZRUL 9X ] ") + chalk.bold.hex(randomColor()).bold(data) +chalk.bold.hex("FFF0000")("\n✧➜  ✧ ✧ ✧ ✧ ✧ ✧ NAZRUL ISLAM LIST (👑)✧ ✧ ✧ ✧ ✧ ✧ ✧ ✧ ✧" ));
       break;
   }
 }
